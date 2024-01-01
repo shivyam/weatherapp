f44f725c3503d5c137bbaf9f4240e8442e2d2bf1
shivyam <144865023+shivyam@users.noreply.github.com>
2023-12-31 23:17:08 -0800

    finished draft

--- weather/views.py
+++ weather/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+import json
+import urllib.request
 
 # Create your views here.
+
+#bfe02dda9c1b329e1f7f717de5a3d4d0
+
+def index(request):
+    if request.method=="POST":
+        city= request.POST['city']
+        res= urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q='+ city+ '&appid=bfe02dda9c1b329e1f7f717de5a3d4d0').read()
+        json_data= json.loads(res)
+        data={
+            "country_code": str(json_data['sys']['country']),
+            "coordinate": str(json_data['coord']['lon']) + ' ' + str(json_data['coord']['lat']),
+            "temp": str(json_data['main']['temp']) + "k",
+            "pressure": str(json_data['main']['pressure']),
+            "humidity": str(json_data['main']['humidity']),
+        }
+    else:
+        data={
+
+        }
+    print(data)
+    return render(request,'index.html', data)
--- weatherapp/settings.py
+++ weatherapp/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'weatherapp'
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'weatherapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/ 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
